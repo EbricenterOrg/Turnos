7d3fba5ac2c2d2aa9fabc483b40edf298d01b4ef
jaim200 <jaime.munoz200@gmail.com>
2014-08-04 11:21:25 -0600

    Prototipo Servidor
    muestra la pantalla en donde se va actualizando los turnos

---
+++ prototipo servidor/Servidor_1/Servidor_1/frmVentana.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Servidor_1
+{
+    partial class frmVentana
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblgestion = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblgestion
+            // 
+            this.lblgestion.AutoSize = true;
+            this.lblgestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblgestion.Location = new System.Drawing.Point(29, 152);
+            this.lblgestion.Name = "lblgestion";
+            this.lblgestion.Size = new System.Drawing.Size(0, 73);
+            this.lblgestion.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmVentana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 463);
+            this.Controls.Add(this.lblgestion);
+            this.Name = "frmVentana";
+            this.Text = "Turno";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblgestion;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ prototipo servidor/Servidor_1/Servidor_1/frmVentana.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+namespace Servidor_1
+{
+    public partial class frmVentana : Form
+    {
+        byte[] b_textorecivido;
+        Socket S;
+        IPEndPoint Direccion;
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();// 
+        PromptBuilder pBuilder = new PromptBuilder();// Promtp que nos dice que hablar
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
+        
+        public frmVentana()
+        {
+            InitializeComponent();
+            S = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Direccion = new IPEndPoint(IPAddress.Any, 1234);
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Hablar()
+        {
+            pBuilder.ClearContent(); // limpia el contenido que sera escrito
+            pBuilder.AppendText( lblgestion.Text);
+            sSynth.GetInstalledVoices(new System.Globalization.CultureInfo("es-ES"));  //ESTE ES PARA IDIOMA
+            sSynth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Teen);  
+            sSynth.Rate = -5;
+            sSynth.Speak(pBuilder);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            S = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Direccion = new IPEndPoint(IPAddress.Any, 1234);  
+            try
+            {
+                S.Bind(Direccion);
+                S.Listen(1);
+               Socket Escuchar = S.Accept();
+                b_textorecivido = new byte[255];
+                int num = Escuchar.Receive(b_textorecivido, 0, b_textorecivido.Length, 0);
+                Array.Resize(ref b_textorecivido,num);
+                lblgestion.Text= "Turno "+Encoding.Default.GetString(b_textorecivido);
+                Hablar();
+                S.Close();
+
+            }
+            catch { 
+            
+            }
+        }
+    }
+}
