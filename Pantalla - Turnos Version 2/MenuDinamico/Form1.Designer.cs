9079b4222522beda0c9e6e04fc87b26e6db8d890
Leonel L. Pazos <leolpazos071@hotmail.com>
2014-08-13 04:40:47 -0600

    Pantalla Clientes -Turnos Version 2.0
    Pantalla con botones dinamicos generados en base a una consulta de la
    Base de Datos

---
+++ Pantalla - Turnos Version 2/MenuDinamico/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace MenuDinamico
+{
+    partial class wfVentanaTicket
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(wfVentanaTicket));
+            this.SuspendLayout();
+            // 
+            // wfVentanaTicket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(792, 566);
+            this.Name = "wfVentanaTicket";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+    }
+}
+
