6f4d1ca13030a8cc41d464ddb96d2b1077a9941e
jaim200 <jaime.munoz200@gmail.com>
2014-08-26 12:45:27 -0600

    Creacion PDF

---
+++ CreacionPDF/CreacionPDF/PDF.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+	
+
+namespace CreacionPDF
+{
+    class PDF
+    {
+        public PDF()
+        { 
+        
+        }
+
+        public void DocPDF(String Cadena)
+        {
+            try
+            {
+                Document documento = new Document();
+                PdfWriter.GetInstance(documento, new FileStream("PDF.pdf",FileMode.OpenOrCreate));
+                documento.Open();
+                documento.Add(new Paragraph(Cadena));
+                documento.Close();
+
+            }
+            catch (Exception)
+            {
+
+                System.Windows.Forms.MessageBox.Show("Error en la generacion", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ CreacionPDF/CreacionPDF/csCreacionPDF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+	
+
+namespace CreacionPDF
+{
+    class PDF
+    {
+        public PDF()
+        { 
+        
+        }
+
+        public void DocPDF(String S_Cadena, String S_NombrePDF)
+        {
+            try
+            {
+                if (!(Directory.Exists(@"C:/Users/Orochi/Desktop/Ticket/")))
+                {
+                    Directory.CreateDirectory(@"C:/Users/Orochi/Desktop/Ticket/");
+                } 
+                Document documento = new Document();
+                PdfWriter.GetInstance(documento, new FileStream(@"C:/Users/Orochi/Desktop/Ticket/" + S_NombrePDF + ".pdf", FileMode.OpenOrCreate));
+                documento.Open();
+                documento.Add(new Paragraph(S_Cadena));
+                documento.Close();
+
+            }
+            catch (Exception)
+            {
+
+                System.Windows.Forms.MessageBox.Show("Error en la generacion", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Error);
+            }
+        }
+    }
+}
