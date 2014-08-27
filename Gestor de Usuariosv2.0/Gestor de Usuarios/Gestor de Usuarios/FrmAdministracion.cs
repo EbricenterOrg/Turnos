ebf7b8340ae191a90b8fc24c19515635050465ab
criportillo <cristianportillobal@hotmail.com>
2014-08-27 16:14:58 -0600

    Gestion Usuarios v2.0

---
+++ Gestor de Usuariosv2.0/Gestor de Usuarios/Gestor de Usuarios/FrmAdministracion.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Gestor_de_Usuarios
+{
+    public partial class FrmAdministracion : Form
+    {
+        private MySqlConnection SQL_Conexion = new MySqlConnection();
+        private String connectionString;
+       
+
+        public FrmAdministracion()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void btnAgregarUser_Click(object sender, EventArgs e)
+        {
+            FrmAgregarUsuarios FrmNuevoUsuario = new FrmAgregarUsuarios();
+            FrmNuevoUsuario.Show();
+        }
+
+        private void btnModificarUser_Click(object sender, EventArgs e)
+        {
+            wfModificarUsuarios wfModificar = new wfModificarUsuarios();
+            wfModificar.Show();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            wfEliminarUsuario wfEliminar = new wfEliminarUsuario();
+            wfEliminar.Show();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string S_Cconn = "Server=192.168.1.100;UID= root; Database= bd_turnos; Password=GrupoSGT014;";
+
+                MySqlConnection SQL_conexion = new MySqlConnection();
+                SQL_conexion.ConnectionString = S_Cconn;
+               
+                SQL_conexion.Open();
+                MySqlDataAdapter SQL_da = new MySqlDataAdapter("Select * from tabm_usuario", SQL_conexion);
+                DataTable DT_dat = new DataTable();
+                SQL_da.Fill(DT_dat);
+                this.dgvUsuarios.DataSource = DT_dat;
+                SQL_conexion.Close();
+ 
+
+                //Numero de usuarios en el sistema
+                SQL_conexion.Open();
+                MySqlCommand SQL_cmd = new MySqlCommand("select count(id_usuario)as Cuenta_users from tabm_usuario ", SQL_conexion);
+                MySqlDataReader dataReader = SQL_cmd.ExecuteReader();
+                while (dataReader.Read()) { lblTotal.Text = dataReader["Cuenta_users"] + ""; }
+                SQL_conexion.Close();
+
+                //Numero Usuarios Administrador
+                SQL_conexion.Open();
+                MySqlCommand SQL_Permiso1 = new MySqlCommand("select count(usur_permiso)as Permiso_user from tabm_usuario where usur_Permiso= 1", SQL_conexion);
+                MySqlDataReader dRPermiso = SQL_Permiso1.ExecuteReader();
+                while (dRPermiso.Read()) { lblAdmin.Text = dRPermiso["Permiso_user"] + ""; }
+                SQL_conexion.Close();
+
+                //numero Usuarios Ventanilla
+                SQL_conexion.Open();
+                MySqlCommand SQL_Permiso2 = new MySqlCommand("select count(usur_permiso)as Permiso_user from tabm_usuario where usur_Permiso= 2", SQL_conexion);
+                MySqlDataReader dRPermiso2 = SQL_Permiso2.ExecuteReader();
+                while (dRPermiso2.Read()) { lblNormales.Text = dRPermiso2["Permiso_user"] + ""; }
+                SQL_conexion.Close();
+
+                //Numero Usuarios Activos
+                SQL_conexion.Open();
+                MySqlCommand SQL_Estado = new MySqlCommand("select count(usur_estado)as usur_estado from tabm_usuario where usur_estado= 'Activo'" , SQL_conexion);
+                MySqlDataReader dREstado = SQL_Estado.ExecuteReader();
+                while (dREstado.Read()) { lblActivos.Text = dREstado["usur_estado"] + ""; }
+                SQL_conexion.Close();
+               
+
+
+            }
+            catch (Exception ei)
+            {
+                MessageBox.Show("Ocurrio un error" + ei.Message);
+            }
+        }
+
+        private void btnBuscarUsuario_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string S_Cconn = "Server=192.168.1.100;UID= root; Database= bd_turnos; Password=GrupoSGT014;";
+
+                MySqlConnection SQL_conexion = new MySqlConnection();
+                SQL_conexion.ConnectionString = S_Cconn;
+                SQL_conexion.Open();
+                MySqlDataAdapter SQL_da = new MySqlDataAdapter("Select * from  tabm_usuario WHERE id_usuario='" + txbBuscarUsuario.Text + "'", SQL_conexion);
+                DataTable DT_dat = new DataTable();
+                SQL_da.Fill(DT_dat);
+                this.dgvUsuarios.DataSource = DT_dat;
+
+                SQL_conexion.Close();
+            }
+            catch (Exception ei)
+            {
+                MessageBox.Show("Ocurrio un error" + ei.Message);
+            }
+        }
+
+        private void FrmAdministracion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+       
+
+    }
+}
---
+++ Gestor de Usuariosv2.0/Gestor de Usuarios/Gestor de Usuarios/wfModificarUsuarios.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Gestor_de_Usuarios
+{
+    public partial class wfModificarUsuarios : Form
+    {
+        private MySqlConnection SQL_Conexion = new MySqlConnection();
+        private String connectionString;
+        private MySqlCommand SQL_cmd;
+
+        public wfModificarUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                String S_cadena = "Server=192.168.1.100;UID= root; Database= bd_turnos; Password=GrupoSGT014;";
+                SQL_Conexion.ConnectionString = S_cadena;
+               /* SQL_Conexion.Open();
+
+
+                SQL_cmd = new MySqlCommand("UPDATE tabm_usuario SET usur_nom=\"" + txtNombre.Text + "\",usur_ape=\"" + txtApellido.Text + "\",usur_ape_cas=\"" + txtApeCasada.Text + "\",usur_permiso=\"" + cboPermiso.Text + "\",usur_dir=\"" + txtDireccion.Text + "\",usur_tel=\"" + txtTelefono.Text + "\",usur_mail=\"" + txtCorreo.Text + "\",usur_pass=\"" + txtPass.Text + "\",usur_estado=\"" + cboEstado.Text + "\" WHERE id_usuario=\"" + txtIdUsuario.Text + "\"", SQL_Conexion);
+               
+                SQL_cmd.ExecuteNonQuery();
+                SQL_Conexion.Close();
+            */
+                //Numero de usuarios en el sistema
+
+                SQL_Conexion.Open();
+                MySqlCommand SQL_cmd = new MySqlCommand("Select * from tab_usuario WHERE id_usuario=\"" + txtIdUsuario.Text + "\"", SQL_conexion);
+                MySqlDataReader dataReader = SQL_cmd.ExecuteReader();
+                while (dataReader.Read()) { lblTotal.Text = dataReader["Cuenta_users"] + ""; }
+                SQL_conexion.Close();
+
+                MessageBox.Show("Usuario modificado exitosamente ", "Modificacion Cuenta Usuario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtIdUsuario.Text = "";
+                txtNombre.Text = "";
+                txtApellido.Text = "";
+                txtApeCasada.Text = "";
+                cboPermiso.Text = "";
+                txtDireccion.Text = "";
+                txtTelefono.Text = "";
+                txtCorreo.Text = "";
+                txtPass.Text = "";
+
+
+         
+        }
+    }
+}
